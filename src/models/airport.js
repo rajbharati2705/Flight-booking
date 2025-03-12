475a33848dcd733c8c9788af72f2da686490488c
Raj bharati <rajbharati@dsmartsys.in>
2025-03-12 10:17:14 +0530

    Added flight model with fk constaint
    ```
    $ npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer
    
    $ npx sequelize db:migrate
    ```

--- src/models/airport.js
+++ src/models/airport.js
@@ -14,7 +14,15 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey:'cityId',
         onDelete:'CASCADE',
         onUpdate:'CASCADE',
-      })
+      });
+      this.hasMany(models.Flight,{
+        foreignKey:'departureAirportId',
+        onDelete:'CASCADE'
+      });
+      this.hasMany(models.Flight,{
+        foreignKey:'arrivalAirportId',
+        onDelete:'CASCADE'
+      });
     }
   }
   Airport.init({
